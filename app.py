6798913be14fbd4e9b259a6247ceb1b967e07ed4
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-06-01 11:43:08 +1000

    Added Marshmallow

--- app.py
+++ app.py
@@ -1,50 +1,81 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+import json
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello"
 
 db = SQLAlchemy(app)
 
+
 class Card(db.Model):
-  __tablename__ = 'cards'
+    __tablename__ = "cards"
 
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(100))
-  description = db.Column(db.Text())
-  date_created = db.Column(db.Date())
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    status = db.Column(db.String(30))
+    date_created = db.Column(db.Date())
 
-@app.cli.command('create')
+
+@app.cli.command("create")
 def create_db():
-  db.drop_all()
-  db.create_all()
-  print('Tables created successfully')
+    db.drop_all()
+    db.create_all()
+    print("Tables created successfully")
 
-@app.cli.command('seed')
+
+@app.cli.command("seed")
 def seed_db():
-  # Create an instance of the Card model in memory
-  card = Card(
-    title = 'Start the project',
-    description = 'Stage 1 - Create an ERD',
-    date_created = date.today()
-  )
+    # Create an instance of the Card model in memory
+    cards = [
+        Card(
+            title="Start the project",
+            description="Stage 1 - Create an ERD",
+            status="Done",
+            date_created=date.today(),
+        ),
+        Card(
+            title="ORM Queries",
+            description="Stage 2 - Implement several queries",
+            status="In Progress",
+            date_created=date.today(),
+        ),
+        Card(
+            title="Marshmallow",
+            description="Stage 3 - Implement jsonify of models",
+            status="In Progress",
+            date_created=date.today(),
+        ),
+    ]
 
-  # Truncate the Card table
-  db.session.query(Card).delete()
-  
-  # Add the card to the session (transaction)
-  db.session.add(card)
+    # Truncate the Card table
+    db.session.query(Card).delete()
 
-  # Commit the transaction to the database
-  db.session.commit()
-  print('Models seeded')
+    # Add the card to the session (transaction)
+    db.session.add_all(cards)
+
+    # Commit the transaction to the database
+    db.session.commit()
+    print("Models seeded")
 
 
-@app.route('/')
+@app.route('/cards')
+def all_cards():
+  # select * from cards;
+  stmt = db.select(Card).order_by(Card.status.desc())
+  cards = db.session.scalars(stmt).all()
+  return json.dumps(cards)
+
+
+@app.route("/")
 def index():
-  return 'Hello World!'
+    return "Hello World!"
 
-if __name__ == '__main__':
-  app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
