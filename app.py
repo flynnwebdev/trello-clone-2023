f2ad3fe731d9ee197bafe497fd91240b25fc505f
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-06-08 12:15:18 +1000

    Added auth_bp

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, abort
 from datetime import date
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from datetime import timedelta
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from os import environ
 from dotenv import load_dotenv
 from models.user import User, UserSchema
 from models.card import Card, CardSchema
 from init import db, ma, bcrypt, jwt
-from blueprints.cli_bp import db_commands
+from blueprints.cli_bp import cli_bp
+from blueprints.auth_bp import auth_bp
 
 load_dotenv()
 
@@ -34,44 +35,10 @@ def unauthorized(err):
     return {'error': 'You must be an admin'}, 401
 
 
-app.register_blueprint(db_commands)
+app.register_blueprint(cli_bp)
+app.register_blueprint(auth_bp)
 
 
-@app.route('/register', methods=['POST'])
-def register():
-    try:
-        # Parse, sanitize and validate the incoming JSON data
-        # via the schema
-        user_info = UserSchema().load(request.json)
-        # Create a new User model instance with the schema data
-        user = User(
-            email=user_info['email'],
-            password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
-            name=user_info['name']
-        )
-
-        # Add and commit the new user
-        db.session.add(user)
-        db.session.commit()
-
-        # Return the new user, excluding the password
-        return UserSchema(exclude=['password']).dump(user), 201
-    except IntegrityError:
-        return {'error': 'Email address already in use'}, 409
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    try:
-        stmt = db.select(User).filter_by(email=request.json['email'])
-        user = db.session.scalar(stmt)
-        if user and bcrypt.check_password_hash(user.password, request.json['password']):
-            token = create_access_token(identity=user.email, expires_delta=timedelta(days=1))
-            return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
-        else:
-            return {'error': 'Invalid email address or password'}, 401
-    except KeyError:
-        return {'error': 'Email and password are required'}, 400
 
 @app.route('/cards')
 @jwt_required()
