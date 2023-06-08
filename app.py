e541c35310689314d01d07ac47490adb9989e14b
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-06-08 13:14:16 +1000

    Created cards blueprint

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, request, abort
-from datetime import date
-from sqlalchemy.exc import IntegrityError
-from datetime import timedelta
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask import Flask
 from os import environ
 from dotenv import load_dotenv
-from models.user import User, UserSchema
-from models.card import Card, CardSchema
 from init import db, ma, bcrypt, jwt
 from blueprints.cli_bp import cli_bp
 from blueprints.auth_bp import auth_bp
+from blueprints.cards_bp import cards_bp
 
 load_dotenv()
 
@@ -23,38 +18,13 @@ ma.init_app(app)
 jwt.init_app(app)
 bcrypt.init_app(app)
 
-def admin_required():
-  user_email = get_jwt_identity()
-  stmt = db.select(User).filter_by(email=user_email)
-  user = db.session.scalar(stmt)
-  if not (user and user.is_admin):
-    abort(401)
-
 @app.errorhandler(401)
 def unauthorized(err):
     return {'error': 'You must be an admin'}, 401
 
-
 app.register_blueprint(cli_bp)
 app.register_blueprint(auth_bp)
-
-
-
-@app.route('/cards')
-@jwt_required()
-def all_cards():
-  admin_required()
-
-  # select * from cards;
-  stmt = db.select(Card).order_by(Card.status.desc())
-  cards = db.session.scalars(stmt).all()
-  return CardSchema(many=True).dump(cards)
-
-
-@app.route("/")
-def index():
-    return "Hello World!"
-
+app.register_blueprint(cards_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
