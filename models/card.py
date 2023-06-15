59680aa7f691ac8bd18faa6ae37a22a59b3c80d6
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-06-15 13:06:10 +1000

    Added custom status validator

--- models/card.py
+++ models/card.py
@@ -1,5 +1,8 @@
 from init import db, ma
-from marshmallow import fields
+from marshmallow import fields, validates_schema
+from marshmallow.validate import Length, OneOf, And, Regexp, ValidationError
+
+VALID_STATUSES = ['To Do', 'Done', 'In Progress', 'Testing', 'Deployed']
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -20,7 +23,22 @@ class CardSchema(ma.Schema):
   # Tell Marshmallow to use UserSchema to serialize the 'user' field
   user = fields.Nested('UserSchema', exclude=['password', 'cards', 'comments'])
   comments = fields.List(fields.Nested('CommentSchema', exclude=['card', 'id']))
-  title = fields.String(required=True)
+  title = fields.String(required=True, validate=And(
+    Length(min=3, error='Title must be at least 3 characters long'),
+    Regexp('^[a-zA-Z0-9 ]+$', error='Only letters, numbers and spaces are allowed')
+  ))
+  description = fields.String(load_default='')
+  status = fields.String(load_default=VALID_STATUSES[0])
+  # status = fields.String(load_default=VALID_STATUSES[0], validate=OneOf(VALID_STATUSES))
+
+  @validates_schema()
+  def validate_status(self, data, **kwargs):
+    status = [x for x in VALID_STATUSES if x.upper() == data['status'].upper()]
+    if len(status) == 0:
+      raise ValidationError(f'Status must be one of: {VALID_STATUSES}')
+
+    data['status'] = status[0]
+
 
   class Meta:
     fields = ('id', 'title', 'description', 'status', 'user', 'comments')
